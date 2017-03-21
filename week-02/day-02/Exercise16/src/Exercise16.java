936c3f30a6bd77fb3ecb86f0d5fa18d844560138
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-21 17:11:49 +0100

    adds Exercise16 to day-02 in week-02

---
+++ week-02/day-02/Exercise16/src/Exercise16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 21..
+ */
+public class Exercise16 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.printf("How many chickens do you have?");
+
+    int chicken = scanner.nextInt();
+
+    System.out.println("How many pigs do you have?");
+
+    int pig = scanner.nextInt();
+
+    System.out.println("The farmer has " + (chicken * 2 + pig * 4) + " legs.");
+    
+  }
+}
