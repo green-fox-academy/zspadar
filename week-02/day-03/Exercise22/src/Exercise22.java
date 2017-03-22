f957f2542b7be43639b4ea27e4fea4047feff4f7
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 11:37:26 +0100

    (feat) second if condition in Exercise 22

--- week-02/day-03/Exercise22/src/Exercise22.java
+++ week-02/day-03/Exercise22/src/Exercise22.java
@@ -19,6 +19,14 @@ public class Exercise22 {
 
     int b = 13;
     String out2 = "";
+
+    if ( b <= 20 && b >= 10) {
+       out2 = "Sweet!" ;
+    } else if ( b < 10 ) {
+       out2 = "More!";
+    } else if (b > 20) {
+      out2 = "Less!"
+    }
     // if b is between 10 and 20 set out2 to "Sweet!"
     // if less than 10 set out2 to "More!",
     // if more than 20 set out2 to "Less!"
