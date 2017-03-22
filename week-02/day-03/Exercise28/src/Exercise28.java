6506d52261ee0c16c5e2b268878156e607e7e1ba
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 13:39:06 +0100

    (feat) whitespaces are at the right place, still in need of stars

--- week-02/day-03/Exercise28/src/Exercise28.java
+++ week-02/day-03/Exercise28/src/Exercise28.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise28 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please give me a number!");
+    int c = scanner.nextInt();
+
+
+    for (int i = 1; i <= (c + 1); i = (i + 2)) {
+      for (int j = 1; j < ((c - i) / 2) + 1; j += 1) {
+        System.out.print(" ");
+      }
+      System.out.println("*");
+    }
+  }
 }
+
+// Write a program that reads a number from the standard input, then draws a
+// pyramid like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as the number was
