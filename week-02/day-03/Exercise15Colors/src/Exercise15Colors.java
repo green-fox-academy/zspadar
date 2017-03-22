864dd73fc3045de2201c308e7ddf9d52d7f4ecc6
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 22:24:43 +0100

    (add) java class to Exercise15Colors

---
+++ week-02/day-03/Exercise15Colors/src/Exercise15Colors.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 22..
+ */
+public class Exercise15Colors {
+}
+// - Create a two dimensional array
+//   which can contain the different shades of specified colors
+// - In `colors[0]` store the shades of green:
+//   `"lime", "forest green", "olive", "pale green", "spring green"`
+// - In `colors[1]` store the shades of red:
+//   `"orange red", "red", "tomato"`
+// - In `colors[2]` store the shades of pink:
+//   `"orchid", "violet", "pink", "hot pink"`
