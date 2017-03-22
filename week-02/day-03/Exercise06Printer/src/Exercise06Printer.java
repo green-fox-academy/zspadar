c72b8ec31464d138414bb785757c1ab94ed234d4
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 22:09:03 +0100

    (feat) printer function works in Exercise06

--- week-02/day-03/Exercise06Printer/src/Exercise06Printer.java
+++ week-02/day-03/Exercise06Printer/src/Exercise06Printer.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise06Printer {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Hello, there! Tell me, what I should print!");
+    String print = scanner.nextLine();
+
+    printer (print);
+  }
+  public static void printer(String computerPrints) {
+    System.out.println(computerPrints);
+  }
 }
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
