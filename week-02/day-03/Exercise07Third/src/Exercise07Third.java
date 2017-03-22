49cebde929313a4c8d14a584f96dfd8c2b5ac43a
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 22:19:39 +0100

    (add) Exercise07Third to day-03 week-02 folder

---
+++ week-02/day-03/Exercise07Third/src/Exercise07Third.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 22..
+ */
+public class Exercise07Third {
+  public static void main(String[] args) {
+    int [] q = {4, 5, 6, 7};
+    System.out.println(q [3]);
+  }
+}
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
