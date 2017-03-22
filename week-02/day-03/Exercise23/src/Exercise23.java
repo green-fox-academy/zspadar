c2ebdcf2916b1600d748f9864a26308fd9dc9fe4
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 12:08:07 +0100

    A(add) Exercise23 to day-03 folder in week-02

---
+++ week-02/day-03/Exercise23/src/Exercise23.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 22..
+ */
+public class Exercise23 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I wont cheat on the exam!");
+    }
+  }
+}
+
