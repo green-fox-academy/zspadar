36e0fb0c347c3203289c3c377e07716785d2774c
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 12:13:48 +0100

    (feat) Exercise24 prints even numbers -yeeeygit status!

--- week-02/day-03/Exercise24/src/Exercise24.java
+++ week-02/day-03/Exercise24/src/Exercise24.java
@@ -2,4 +2,9 @@
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise24 {
+  public static void main(String[] args) {
+        for (int i = 0; i < 501; i += 2) {
+              System.out.println(i);
+            }
+  }
 }
