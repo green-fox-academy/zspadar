6c65e36f2ceec60a2a606d6d7dcf1d487dc4a86e
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 16:38:29 +0100

    (function) factorio works in Exercise05

--- week-02/day-03/Exercise05factorio/src/Exercise05factorio.java
+++ week-02/day-03/Exercise05factorio/src/Exercise05factorio.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise05factorio {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int ui = scanner.nextInt();
+    System.out.println(factorio(ui));
+  }
+  public static int factorio (int numbers) {
+    int result = 1;
+    for (int i = 1; i < numbers + 1; i++) {
+      result = result * i;
+    }
+    return result;
+  }
 }
+// - Create a function called `factorio`
+//   that returns it's input's factorial
