3d3835c8e328aae97c23fe9dfa5da08c35b3e23f
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 12:22:47 +0100

    (feat) in Exercise 25, asking for two numbers

--- week-02/day-03/Exercise25/src/Exercise25.java
+++ week-02/day-03/Exercise25/src/Exercise25.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise25 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please give me a number!");
+    int ui1 = scanner.nextInt();
+    System.out.println("Please, give mi another number!");
+    int ui2 = scanner.nextInt();
+  }
 }
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+//
+// example:
+//
+// first number: 3, second number: 6, should print:
+//
+// 3
+// 4
+// 5
