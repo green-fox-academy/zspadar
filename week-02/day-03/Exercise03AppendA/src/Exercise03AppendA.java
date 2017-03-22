11c1aeadabe73273d9b601c914eba16eb63ba843
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 15:39:39 +0100

    (add) Exercise03 function appendA

---
+++ week-02/day-03/Exercise03AppendA/src/Exercise03AppendA.java
@@ -0,0 +1,16 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 22..
+ */
+public class Exercise03AppendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    System.out.println(appendA(am));
+  }
+  public static String appendA(String am) {
+    return am = am + "a";
+  }
+}
+// - Create a string variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input
+//   and appends an 'a' character to its end
+// - Print the result of `appendA(am)`
