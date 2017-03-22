835872e58bfbcae5a416777d4046e5239dcadf77
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 16:12:23 +0100

    (feat) sum function is Exercise04

--- week-02/day-03/Exercise04sum/src/Exercise04sum.java
+++ week-02/day-03/Exercise04sum/src/Exercise04sum.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise04sum {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int ui = scanner.nextInt();
+    System.out.println(sum(ui));
+  }
+  public static int sum(int numbers) {
+    int result = 0;
+    for ( int i = 0; i < numbers; i ++) {
+      result = result + i;
+    }
+    return result;
+  }
 }
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter
