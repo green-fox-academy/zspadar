11a458078f28c120f05cbd429b0d698fd18c1c4c
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 14:56:29 +0100

    still not working diamond

--- week-02/day-03/Exercise29/src/Exercise29.java
+++ week-02/day-03/Exercise29/src/Exercise29.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise29 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please give me a number!");
+    int c = scanner.nextInt();
+
+
+    for (int i = 1; i <= (c + 1); i = (i + 2)) {
+      for (int j = 0; j < (((c - i) / 2) + 1); j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+
+      for (int j = 0; j < (((c - i) / 2) + 1); j++) {
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+    for (int i = (c - 2); i > 0; i --) {
+      for (int j = 0; j < (((c - i) / 2) + 1); j++)  {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < (((c - i) / 2) + 1); j++) {
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
 }
+
+
+
