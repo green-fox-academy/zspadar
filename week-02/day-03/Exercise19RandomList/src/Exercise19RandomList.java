5f53c72291257ad0b28595575e0b04056132c139
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 23:29:32 +0100

    (add) java class to Exercise19RandomList

---
+++ week-02/day-03/Exercise19RandomList/src/Exercise19RandomList.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 22..
+ */
+public class Exercise19RandomList {
+}
+//- Create an array variable named `aj`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Reverse the order of the elements in `aj`
+// - Print the elements of the reversed `aj`
