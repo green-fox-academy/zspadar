5393536db54c76567ddd746ced9420adf3772480
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 15:31:00 +0100

    (add) greeting return function

--- week-02/day-03/Exercise02Greet/src/Exercise02Greet.java
+++ week-02/day-03/Exercise02Greet/src/Exercise02Greet.java
@@ -2,4 +2,18 @@
  * Created by zsuzsanna.padar on 2017. 03. 22..
  */
 public class Exercise02Greet {
+  public static void main(String[] args) {
+    String al = "Greenfox";
+    System.out.println(greet(al));
+  }
+  public static String greet(String al) {
+    return "Greetings dear, " + al;
+  }
 }
+//  From now on, create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
