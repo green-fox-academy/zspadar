c52db2f8ae36a23ee397b994350189362544ba12
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-22 21:16:38 +0100

    (feat) working diamond

--- week-02/day-03/Exercise29/src/Exercise29.java
+++ week-02/day-03/Exercise29/src/Exercise29.java
@@ -11,11 +11,20 @@ public class Exercise29 {
     int c = scanner.nextInt();
 
 
-    for (int i = 0; i < c / 2 + 1; i ++) {
-      for (int j = 0; j < c / 2 - i; j ++) {
+    for (int i = 0; i < c / 2 + 1; i++) {
+      for (int j = 0; j < c / 2 - i; j++) {
         System.out.print(" ");
       }
-      for (int s = 0; s < i * 2 +1; s++) {
+      for (int s = 0; s < i * 2 + 1; s++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (int i = c / 2; i > 0; i --) {
+      for (int j = c/ 2 - i + 1; j > 0; j--) {
+        System.out.print(" ");
+      }
+      for (int s = i * 2 -1; s > 0; s --) {
         System.out.print("*");
       }
       System.out.println();
