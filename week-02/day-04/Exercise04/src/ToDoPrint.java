02181c0f22b66ecfa3d5ced3e5d6f26d9e5862e4
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-23 13:07:30 +0100

    (feat) concat in concat

--- week-02/day-04/Exercise04/src/ToDoPrint.java
+++ week-02/day-04/Exercise04/src/ToDoPrint.java
@@ -2,4 +2,22 @@
  * Created by zsuzsanna.padar on 2017. 03. 23..
  */
 public class ToDoPrint {
+  public static void main(String... args){
+    String todoText = " - Buy milk\n";
+
+    todoText = ("My todo:\n").concat("  - Buy milk\n").concat("  - Download games\n").concat("        -Diablo");
+
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected outpt:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    System.out.println(todoText);
+  }
 }
