60f195186bc7e499f5e4461022fd11abd0d8e394
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-23 14:08:20 +0100

    complete exercise - semicolons are dangerous

--- week-02/day-04/Exercise05/src/Reverse.java
+++ week-02/day-04/Exercise05/src/Reverse.java
@@ -5,13 +5,15 @@ public class Reverse {
   public static void main(String... args) {
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
+
     System.out.println(reverse(reversed));
+
   }
 
   public static String reverse(String sentence) {
     String firstchar = " ";
-    for (int i = sentence.length() - 1; i > 0; i--); {
-      firstchar = firstchar + sentence.charAt(0);
+    for (int i = sentence.length() - 1; i >= 0; i--) {
+      firstchar = firstchar + sentence.charAt(i);
     }
     return firstchar;
   }
