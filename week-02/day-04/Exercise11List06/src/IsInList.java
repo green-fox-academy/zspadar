2e398e84e95c1ce896008c5ce3be28c8fb1eb5c2
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-23 21:50:50 +0100

    (add) one possible solution for IsInList

--- week-02/day-04/Exercise11List06/src/IsInList.java
+++ week-02/day-04/Exercise11List06/src/IsInList.java
@@ -7,10 +7,13 @@ public class IsInList{
   public static void main(String... args){
     ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
 
-    if (list.containsAll(list.indexOf(1 ))) {
+    if (list.containsAll(new ArrayList<Integer>(Arrays.asList(4,8,12,16)))) {
       System.out.println("true");
     } else {
       System.out.println("false");
     }
   }
 }
+// Check if list contains all of the following elements: 4,8,12,16
+// Print "true" if it contains all, otherwise print "false"
+// Can you do both the different approaches you tried in the previous one?
