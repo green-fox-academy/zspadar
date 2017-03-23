7d8bb6f76ff67b87e831eaa2a9b313d5c999c0eb
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-23 22:04:29 +0100

    (add) java class to Exercise QuoteSwap

---
+++ week-02/day-04/Exercise12List07/src/QuoteSwap.java
@@ -0,0 +1,16 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 23..
+ */
+import java.util.*;
+
+public class QuoteSwap{
+  public static void main(String... args){
+    ArrayList<String> list = new ArrayList<String>(Arrays.asList("What", "I", "do", "create,", "I", "cannot", "not", "understand."));
+
+    // Accidentally I messed up this quote from Richard Feynman.
+    // Two words are out of place
+    // Your task is to fix it by swapping the right words with code
+
+    // Also, print the sentence to the output with spaces in between.
+  }
+}
