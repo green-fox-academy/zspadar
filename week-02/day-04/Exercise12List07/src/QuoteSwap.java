ea1a6d659cb13cf530a5e18248cc658d8b8c75ba
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-23 22:21:55 +0100

    (add) QuoteSwap function -yihhaa

--- week-02/day-04/Exercise12List07/src/QuoteSwap.java
+++ week-02/day-04/Exercise12List07/src/QuoteSwap.java
@@ -7,6 +7,12 @@ public class QuoteSwap{
   public static void main(String... args){
     ArrayList<String> list = new ArrayList<String>(Arrays.asList("What", "I", "do", "create,", "I", "cannot", "not", "understand."));
 
+    String temp1 = list.get(2);
+    String temp2 = list.get(5);
+    list.set(2, temp2);
+    list.set(5, temp1);
+    System.out.println(String.join(" ", list));
+    // 2,5; 5, 2
     // Accidentally I messed up this quote from Richard Feynman.
     // Two words are out of place
     // Your task is to fix it by swapping the right words with code
