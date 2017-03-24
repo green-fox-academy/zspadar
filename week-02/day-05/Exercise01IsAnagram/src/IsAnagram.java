adad797837a321049aa8fd921005f66692229a96
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-24 10:51:34 +0100

    (add) anagram function to Exercise01

--- week-02/day-05/Exercise01IsAnagram/src/IsAnagram.java
+++ week-02/day-05/Exercise01IsAnagram/src/IsAnagram.java
@@ -2,4 +2,12 @@
  * Created by zsuzsanna.padar on 2017. 03. 24..
  */
 public class IsAnagram {
+  public static void main(String[] args) {
+    System.out.println(isAnagram( "dog", "god"));
+    System.out.println(isAnagram("green", "fox"));
+  }
+
+  public static boolean isAnagram(String word1, String word2) {
+    return word1.equals(new StringBuffer(word2).reverse().toString());
+  }
 }
