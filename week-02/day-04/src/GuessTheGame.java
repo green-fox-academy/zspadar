e1512cacd95f78b8a4e6215b503006a58390e26b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-26 11:46:15 +0200

    (add) Guess The Game from day02

--- week-02/day-04/src/GuessTheGame.java
+++ week-02/day-04/src/GuessTheGame.java
@@ -4,24 +4,25 @@ import java.util.Scanner;
  * Created by zsuzsanna.padar on 2017. 03. 23..
  */
 public class GuessTheGame {
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("I will think of a number, your job is to figure it out! Shall we start?");
-      int userInput = scanner.nextLine();
+  public static void main(String[] args) {
+    int stored = 147;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("I will think of a number, your job is to figure it out! Shall we start?");
+    int userInput = scanner.nextInt();
 
-      if (userInput !=) {
-        if (userInput <) {
-          System.out.println("The stored number is higher");
-        }
-        if (userInput > ) {
-          System.out.println("The stored number is lower");
-        }
-        else {
-          System.out.println("You found the number: " + );
-        }
+
+    while (userInput != stored) {
+      if (userInput < stored) {
+        System.out.println("The stored number is higher");
+      } else {
+        System.out.println("The stored number is lower");
       }
+      userInput = scanner.nextInt();
     }
+    System.out.println("You found the number: " + stored);
   }
+}
+
 // Write a program that stores a number, and the user has to figure it out.
 // The user can input guesses, after each guess the program would tell one
 // of the following:
@@ -29,4 +30,4 @@ public class GuessTheGame {
 // The stored number is higher
 // The stried number is lower
 // You found the number: 8
-}
+
--- week-02/day-05/Exercise03SearchPalindrome/src/SearchPalindrome.java
+++ week-02/day-05/Exercise03SearchPalindrome/src/SearchPalindrome.java
@@ -2,4 +2,19 @@
  * Created by zsuzsanna.padar on 2017. 03. 24..
  */
 public class SearchPalindrome {
+  public static void main(String[] args) {
+    System.out.println(searchPalindrome("dog goat dad duck doodle never"));
+    System.out.println(searchPalindrome("apple"));
+    System.out.println(searchPalindrome(" "));
+  }
+  public static String searchPalindrome(String parts) {
+    for (int i = 0; i < parts.length(); i++) {
+      public static Boolean searchPalindrome(String particle) {
+        return (parts.indexOf(searchPalindrome([i], parts.length())) &&
+        parts.equals(new StringBuffer(parts).reverse().toString()));
+      }
+      return parts;
+    }
+  }
 }
+
