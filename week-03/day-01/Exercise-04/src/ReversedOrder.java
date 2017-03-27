fb371e9f7a3e6b67a43363983e76224773b90887
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-27 23:33:14 +0200

    (add)reversed list in lines to Exercise-04

--- week-03/day-01/Exercise-04/src/ReversedOrder.java
+++ week-03/day-01/Exercise-04/src/ReversedOrder.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,9 +13,23 @@ public class ReversedOrder {
     try {
       Path path = Paths.get("assets/reversed-order.txt");
       List<String> lines = Files.readAllLines(path);
+      printReversedToLines(reversed(lines));
     } catch (IOException ex) {
       System.out.println("Reading error!");
     }
     System.out.println("Readable file! :)");
+  }
+  public static List<String> reversed (List<String> reversedSenteces) {
+    ArrayList<String> reversedSentence = new ArrayList<>();
+    for (int i = reversedSenteces.size() - 1; i >= 0; i--) {
+      reversedSentence.add(reversedSenteces.get(i));
+    }
+    return reversedSentence;
+  }
+  public static void printReversedToLines (List<String> reversedList) {
+    for (int i = 0; i < reversedList.size(); i++) {
+      System.out.println(reversedList.get(i));
+    }
+
   }
 }
