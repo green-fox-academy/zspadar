3605efce143b2dd37098a6aa8d4b65fbfac727e2
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-27 21:14:59 +0200

    (add) ReversedOrder java class to Exercise-04

--- week-03/day-01/Exercise-03/src/ReversedLines.java
+++ week-03/day-01/Exercise-03/src/ReversedLines.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,10 +13,19 @@ public class ReversedLines {
     try {
       Path path = Paths.get("reversed-lines.txt");
       List<String> lines = Files.readAllLines(path);
-     // System.out.println(reverse(lines));
+      System.out.println(reverse(lines));
     }catch (IOException ex) {
       System.out.println("Unable to read!");
     }
     System.out.println("Readable!");
+  }
+
+
+  public static List<String> reverse (List<String> sentence){
+    ArrayList<String> reversedSentence = new ArrayList<>();
+    for (int i = sentence.size() - 1; i >= 0; i--) {
+      reversedSentence.add(sentence.get(i));
+    }
+    return reversedSentence;
   }
 }
