ef6fbca248df5e9a6f4421991c1c33d788418291
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-27 17:05:50 +0200

    (add) reversed-lines.txt is readable

--- week-03/day-01/Exercise-03/src/ReversedLines.java
+++ week-03/day-01/Exercise-03/src/ReversedLines.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 27..
  */
 public class ReversedLines {
+  public static void main(String[] args) {
+    try {
+      Path path = Paths.get("reversed-lines.txt");
+      List<String> lines = Files.readAllLines(path);
+     // System.out.println(reverse(lines));
+    }catch (IOException ex) {
+      System.out.println("Unable to read!");
+    }
+    System.out.println("Readable!");
+  }
 }
