ff8bbc599580fb29c871ef16103803df39566c78
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-28 00:24:06 +0200

    (add) complete Exercise to HeWillNever

--- week-02/day-04/Exercise14Map01/src/HeWillNever.java
+++ week-02/day-04/Exercise14Map01/src/HeWillNever.java
@@ -9,22 +9,28 @@ public class HeWillNever {
     String out = "";
     int[] notSoCrypticMessage = {1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11};
 
-    Map<Integer, String> map = new HashMap<Integer, String>();
+    Map<Integer, String> hmap = new HashMap<Integer, String>();
 
-    map.put(7, "run around and desert you");
-    map.put(50, "tell a lie and hurt you");
-    map.put(49, "make you cry,");
-    map.put(2, "let you down");
-    map.put(12, "give you up,");
-    map.put(1, "Never gonna");
-    map.put(11, "\n");
-    map.put(3, "say goodbye");
+    hmap.put(7, "run around and desert you");
+    hmap.put(50, "tell a lie and hurt you");
+    hmap.put(49, "make you cry,");
+    hmap.put(2, "let you down");
+    hmap.put(12, "give you up,");
+    hmap.put(1, "Never gonna");
+    hmap.put(11, "\n");
+    hmap.put(3, "say goodbye");
+
+    for (int i = 0; i < notSoCrypticMessage.length; i++) {
+      if (hmap.containsKey(notSoCrypticMessage[i]))
+      out = out + " " + hmap.get(notSoCrypticMessage[i]);
+    }
+
+    System.out.println(out);
 
 
     // Things are a little bit messed up
     // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
     // Assemble the fragments into the out variable
 
-    System.out.println(out);
   }
 }
