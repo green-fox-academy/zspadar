b2191b34141ce2d19479bbfb61e3e29250915d75
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-28 20:27:05 +0200

    (feature) encoded-lines.txt is readable in Exercise-05

--- week-03/day-01/Exercise-05/src/EncodedLines.java
+++ week-03/day-01/Exercise-05/src/EncodedLines.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 28..
  */
 public class EncodedLines {
+  public static void main(String[] args) {
+    try{
+      Path path = Paths.get("assets/encoded-lines.txt");
+      List<String> lines = Files.readAllLines(path);
+    } catch (IOException ex) {
+      System.out.println("Error reading file! :((((");
+    }
+    System.out.println("File is perfectly readable, yeah!");
+  }
 }
+
+
