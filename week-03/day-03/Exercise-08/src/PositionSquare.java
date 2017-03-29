f2824e050a69a7e44a5560cc395ad2f0dfa3c148
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 11:56:53 +0200

    (feature) 2 squares arte ready, one more to go

--- week-03/day-03/Exercise-08/src/PositionSquare.java
+++ week-03/day-03/Exercise-08/src/PositionSquare.java
@@ -1,5 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 29..
  */
 public class PositionSquare {
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    drawSquare(0,0, graphics);
+    drawSquare(55,55, graphics);
+    
+
+
+
+  }
+  public static void drawSquare(int x, int y, Graphics graphics) {
+    graphics.fillRect(x, y, 50, 50);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
 }
