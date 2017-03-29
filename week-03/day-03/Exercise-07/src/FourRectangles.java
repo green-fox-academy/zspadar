e868f2dc5b6b162dadd7376d12a6c12e015af336
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 11:43:17 +0200

    (feature) draws four triangles in Exercise-07

--- week-03/day-03/Exercise-07/src/FourRectangles.java
+++ week-03/day-03/Exercise-07/src/FourRectangles.java
@@ -11,6 +11,15 @@ public class FourRectangles {
     // draw four different size and color rectangles.
     graphics.setColor(Color.GREEN);
     graphics.fillPolygon(new int[] {4, 16, 20}, new int[] {0, 20, 20}, 3);
+
+    graphics.setColor(Color.YELLOW);
+    graphics.fillPolygon(new int[] {80, 40, 70}, new int[] {0, 30, 30}, 3);
+
+    graphics.setColor(Color.RED);
+    graphics.fillPolygon(new int[] {189, 72, 98}, new int[] {0, 40, 40}, 3);
+
+    graphics.setColor(Color.BLUE);
+    graphics.fillPolygon(new int[] {250, 180, 256}, new int[] {0, 50, 50}, 3);
   }
 
   //    Don't touch the code below
