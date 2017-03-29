11c874075f4b078b4d4b5a4a8d0b561d820e5b7c
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 10:17:16 +0200

    (add) Diagonal java class to Exercise-03

---
+++ week-03/day-03/Exercise-03/src/Diagonals.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 29..
+ */
+public class Diagonals {
+  public static void mainDraw(Graphics graphics){
+    // draw the canvas' diagonals in green.
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
