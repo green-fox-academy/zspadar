744ca990502f937c4e542f26cc1f7c55de774318
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 11:12:21 +0200

    (function) draws 3 horizontal lines, each is 50 pixels long

---
+++ week-03/day-03/Exercise-05/src/HorizontalLines.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 29..
+ */
+public class HorizontalLines {
+  public static void mainDraw(Graphics graphics){
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a 50 long horizontal line from that point.
+    // draw 3 lines with that function.
+    drawLine(20, 40, graphics);
+    drawLine(2,80, graphics);
+    drawLine(17,90,graphics);
+  }
+  
+  public static  void drawLine(int x, int y, Graphics graphics) {
+    graphics.drawLine(x, y, x + 50, y + 0);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
