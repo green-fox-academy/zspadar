e3961d1573908aa4f1f903bc7fe936a0bf24b558
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 11:23:41 +0200

    (feature) green square in the middle of the canvas in Exercise-06

--- week-03/day-03/Exercise-06/src/CenteredSquare.java
+++ week-03/day-03/Exercise-06/src/CenteredSquare.java
@@ -9,9 +9,8 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class CenteredSquare {
   public static void mainDraw(Graphics graphics){
     // draw a green 10x10 square to the canvas' center.
-
-
-
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(145,145,10,10);
   }
 
   //    Don't touch the code below
