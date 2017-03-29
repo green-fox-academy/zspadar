04fa4f366d04c485172099ff6e987c19800a9957
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-29 12:17:27 +0200

    (feature) drawSquare function is working in Exercise-09

--- week-03/day-03/Exercise-09/src/CenterBoxFunction.java
+++ week-03/day-03/Exercise-09/src/CenterBoxFunction.java
@@ -1,5 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  * Created by zsuzsanna.padar on 2017. 03. 29..
  */
 public class CenterBoxFunction {
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
+    drawSquare(40, graphics);
+
+
+
+  }
+
+  public static void drawSquare(int sizexy, Graphics graphics) {
+  graphics.drawRect(150 - sizexy / 2, 150 - sizexy /2, sizexy, sizexy );
+  }
+
+
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
 }
