d5bf08072ce313823a888e1d5b31a86317e94d51
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 10:40:20 +0200

    (add) recursive sum digit

---
+++ week-03/day-04/Exercise-03/src/SumDigit.java
@@ -0,0 +1,19 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 03. 30..
+ */
+public class SumDigit {
+  public static void main(String[] args) {
+    System.out.println(sumDigit(1254));
+  }
+  
+  public static int sumDigit(int n) {
+    int rightMost = n % 10;
+    int left = n / 10;
+    if (left == 0) {
+      return rightMost;
+    } else {
+      return rightMost + sumDigit (left);
+    }
+
+  }
+}
