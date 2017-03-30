9f18668d55cf0e2f880ccfcaaa5903859b6a84cd
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 10:47:11 +0200

    (feature) x power y recursively

--- week-03/day-04/Exercise-04/src/Power.java
+++ week-03/day-04/Exercise-04/src/Power.java
@@ -2,5 +2,15 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class Power {
+  public static void main(String[] args) {
+    System.out.println(powerN(2, 4));
+  }
   
+  public static int powerN(int x, int y) {
+    if ( y == 1 ) {
+      return x;
+    } else {
+      return x * powerN(x, y-1);
+    }
+  }
 }
