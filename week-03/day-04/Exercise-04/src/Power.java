53ae4678bb28c41590040fd44f46da6ea17cc9b8
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 10:49:08 +0200

    (add) Bunny java class to Exercise-05

--- week-03/day-04/Exercise-04/src/Power.java
+++ week-03/day-04/Exercise-04/src/Power.java
@@ -5,7 +5,7 @@ public class Power {
   public static void main(String[] args) {
     System.out.println(powerN(2, 4));
   }
-  
+
   public static int powerN(int x, int y) {
     if ( y == 1 ) {
       return x;
