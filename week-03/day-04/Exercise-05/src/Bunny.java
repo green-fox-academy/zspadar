efe7f982e56a810bc72667611439b15d6f64f002
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 10:51:38 +0200

    (feature) bunny ear counter is working

--- week-03/day-04/Exercise-05/src/Bunny.java
+++ week-03/day-04/Exercise-05/src/Bunny.java
@@ -2,4 +2,15 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class Bunny {
+  public static void main(String[] args) {
+    System.out.println(bunnies(11));
+  }
+  
+  public static int bunnies(int n) {
+    if (n == 1) {
+      return  2;
+    } else {
+      return 2 + bunnies(n - 1);
+    }
+  }
 }
