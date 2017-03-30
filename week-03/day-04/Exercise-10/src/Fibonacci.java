cc42dd91eb535722d870835cbf1d889a84966e4a
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 13:16:20 +0200

    (add) fibonacci is working

--- week-03/day-04/Exercise-10/src/Fibonacci.java
+++ week-03/day-04/Exercise-10/src/Fibonacci.java
@@ -2,4 +2,17 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class Fibonacci {
+  public static void main(String[] args) {
+    System.out.println(fibonacci(8));
+  }
+
+  public static int fibonacci(int n) {
+    if (n == 1) {
+      return 0;
+      } else if (n == 2) {
+        return 1;
+        } else {
+          return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+  }
 }
