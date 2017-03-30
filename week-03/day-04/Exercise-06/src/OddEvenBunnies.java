b7a84cb1f5f94623743733b80fe5f17a7ef4fad3
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 11:00:43 +0200

    (feature) odd even bummy ear counter is working

--- week-03/day-04/Exercise-06/src/OddEvenBunnies.java
+++ week-03/day-04/Exercise-06/src/OddEvenBunnies.java
@@ -2,4 +2,19 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class OddEvenBunnies {
+  public static void main(String[] args) {
+    System.out.println(oddEvenBunnyEarCounter(12));
+  }
+
+  public static int oddEvenBunnyEarCounter(int n) {
+    if (n == 1) {
+      return 2;
+    } else {
+      if (n % 2 == 0) {
+        return 2 + oddEvenBunnyEarCounter(n - 1);
+      } else {
+        return 3 + oddEvenBunnyEarCounter(n - 1);
+      }
+    }
+  }
 }
