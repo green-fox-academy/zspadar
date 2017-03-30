11750b0d9024399e14db265f1fd6c5ae1ab11fb9
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 12:51:48 +0200

    (add) String again exercise

--- week-03/day-04/Exercise-08/src/StringsAgain.java
+++ week-03/day-04/Exercise-08/src/StringsAgain.java
@@ -2,4 +2,18 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class StringsAgain {
+  public static void main(String[] args) {
+    System.out.println(removeXes("Stxxerrtglxexs"));
+  }
+
+  public static String removeXes(String word) {
+    if (word.length() == 0) {
+      return word;
+    }
+    if (word.charAt(0) == 'x') {
+      return  removeXes(word.substring(1));
+    } else {
+      return  word.charAt(0) + removeXes(word.substring(1));
+    }
+  }
 }
