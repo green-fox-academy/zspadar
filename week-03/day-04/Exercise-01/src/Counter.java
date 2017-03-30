bda0b0f6dbd7f5fd487e5434d1a595c4d31ca4b0
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-03-30 10:21:24 +0200

    (feature) Countdown recursive

--- week-03/day-04/Exercise-01/src/Counter.java
+++ week-03/day-04/Exercise-01/src/Counter.java
@@ -2,4 +2,16 @@
  * Created by zsuzsanna.padar on 2017. 03. 30..
  */
 public class Counter {
+  public static void main(String[] args) {
+   countDown(11);
+  }
+  
+  public static  void countDown(int n) {
+    if (n == 0) {
+      return;
+    } else {
+      System.out.println(n);
+      countDown(n - 1);
+    }
+  }
 }
