3039912095817e7aeb924fb57716d0ed28245e89
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-03 15:33:28 +0200

    (add) Class to Exercise-09

--- week-04/day-01/Exercise-09/src/Class.java
+++ week-04/day-01/Exercise-09/src/Class.java
@@ -3,6 +3,11 @@
  */
 public class Class {
   public static void main(String[] args) {
-    
+    Student student = new Student();
+    Teacher teacher = new Teacher();
+
+    System.out.println(teacher.teach(student));
+    System.out.println(student.question(teacher));
+
   }
 }
