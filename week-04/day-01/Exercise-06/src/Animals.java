23b9f5ef2b7b8739487633217d5f4b1d83d564d2
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-03 14:59:39 +0200

    (add) Animals are working

--- week-04/day-01/Exercise-06/src/Animals.java
+++ week-04/day-01/Exercise-06/src/Animals.java
@@ -2,4 +2,18 @@
  * Created by zsuzsanna.padar on 2017. 04. 03..
  */
 public class Animals {
+  public static void main(String[] args) {
+    Animal giraffe = new Animal();
+    Animal elephant = new Animal();
+
+    System.out.println(giraffe.thirst);
+    giraffe.play();
+    System.out.println(giraffe.thirst);
+    System.out.println(giraffe.hunger);
+    System.out.println(elephant.thirst + elephant.hunger);
+    elephant.drink();
+    System.out.println(elephant.thirst);
+    giraffe.eat();
+    System.out.println(giraffe.hunger);
+  }
 }
