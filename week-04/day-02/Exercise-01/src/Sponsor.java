4badb3b531499c0278b3edc4e6fe3a19045663ca
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-04 13:23:19 +0200

    (add) Sponsor fields and methods to java class

---
+++ week-04/day-02/Exercise-01/src/Sponsor.java
@@ -0,0 +1,34 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 04..
+ */
+public class Sponsor extends Person {
+  private  String company;
+  private int hiredStudents;
+
+  @Override
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.getName() + ", a" + this.getAge() + " year old" + this.getGender() + "," +
+            "who represents " + company + " and hired " + hiredStudents + " so far.");
+  }
+
+  public void hire(int numberOfStudents) {
+    this.hiredStudents += 1;
+  }
+
+  Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+  }
+
+  Sponsor() {
+    super();
+    company = "Google";
+    this.hiredStudents = 0;
+  }
+  
+}
