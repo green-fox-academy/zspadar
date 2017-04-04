b9246341c09f25d0675ff9cbf7780689a47ad69b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-04 17:22:18 +0200

    (add) plants to main method

--- week-04/day-02/Exercise-02/src/TheGardenApplication.java
+++ week-04/day-02/Exercise-02/src/TheGardenApplication.java
@@ -2,4 +2,10 @@
  * Created by zsuzsanna.padar on 2017. 04. 04..
  */
 public class TheGardenApplication {
+  public static void main(String[] args) {
+    Plants flower1 = new Flowers("yellow", 34, 75 );
+    Plants flower2 = new Flowers("blue", 2, 75);
+    Plants tree1 = new Trees("purple", 8, 40);
+    Plants tree2 = new Trees("orange", 6, 40);
+  }
 }
