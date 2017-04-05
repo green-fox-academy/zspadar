f7f0697b621a39ca94a382b407958ff6256fc890
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-05 15:11:19 +0200

    (add) (function) fibonacci to java class

--- week-04/day-03/Exercise-05/src/Fibonacci.java
+++ week-04/day-03/Exercise-05/src/Fibonacci.java
@@ -2,4 +2,13 @@
  * Created by zsuzsanna.padar on 2017. 04. 05..
  */
 public class Fibonacci {
+  public static int fibonacci(int n) {
+    if (n == 1) {
+      return 0;
+    } else if (n == 2) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
 }
