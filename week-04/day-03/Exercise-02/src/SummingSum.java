f0f649c7d5245b355445de55e19c4479829c44e9
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-05 12:03:41 +0200

    (add) first test works

--- week-04/day-03/Exercise-02/src/SummingSum.java
+++ week-04/day-03/Exercise-02/src/SummingSum.java
@@ -1,19 +1,15 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 05..
  */
 public class SummingSum {
-  private ArrayList<Integer> myList;
-
   public SummingSum() {
-    this.myList = new ArrayList<>();
-    myList.add(54);
-    myList.add(12);
-    myList.add(34);
   }
 
-  public int sumItUp() {
+
+  public int sumItUp(List<Integer> myList) {
     int sum = 0;
     for (int i : myList) {
       sum = sum + i;
@@ -21,13 +17,9 @@ public class SummingSum {
     return sum;
   }
 
-  public void addIntegers (Integer numbers) {
-    myList.add(numbers);
-  }
 
-  public ArrayList<Integer> getMyList() {
-    return myList;
-  }
+
+
 
 
 
--- week-04/day-03/Exercise-02/test/SummingSumTest.java
+++ week-04/day-03/Exercise-02/test/SummingSumTest.java
@@ -1,8 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 05..
  */
 public class SummingSumTest {
 
+  SummingSum summingSum = new SummingSum();
+
+
+  @Before
+  public void beforeEachTest(){
+    System.out.println("Before");
+  }
+
+
+  @Test
+  public void testIfSumIsSum() throws Exception {
+    List<Integer> myList = new ArrayList<>(Arrays.asList(12,14,14));
+    summingSum.sumItUp(myList);
+    assertEquals(summingSum.sumItUp(myList), 40);
+
+
+
+
+  }
 }
