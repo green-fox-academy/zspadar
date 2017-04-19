f4b98ea86d9d91ee8aace3c2b0a1edda66052331
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-19 13:17:39 +0200

    (feat) random Color to CarColor in Exercise-01

--- week-06/day-03/Exercise-01/src/CarColor.java
+++ week-06/day-03/Exercise-01/src/CarColor.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 /**
  * Created by zsuzsanna.padar on 2017. 04. 19..
  */
 public enum CarColor {
+  RED, BLUE, GREEN;
+
+  public static CarColor getRandColor() {
+    Random random = new Random();
+    int getRand = random.nextInt(CarColor.values().length);
+    return CarColor.values()[getRand];
+  }
 }
