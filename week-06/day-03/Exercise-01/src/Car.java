303c0e38c27c84d895c23912b42ac98a600fd2bb
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-19 13:32:43 +0200

    (add) field end constructor to Car class

--- week-06/day-03/Exercise-01/src/Car.java
+++ week-06/day-03/Exercise-01/src/Car.java
@@ -2,4 +2,11 @@
  * Created by zsuzsanna.padar on 2017. 04. 19..
  */
 public class Car {
+  public CarType carType;
+  public CarColor carColor;
+
+  public Car(CarType carType, CarColor carColor) {
+    this.carType = carType;
+    this.carColor = carColor;
+  }
 }
--- week-06/day-03/Exercise-01/src/CarType.java
+++ week-06/day-03/Exercise-01/src/CarType.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 /**
  * Created by zsuzsanna.padar on 2017. 04. 19..
  */
 public enum CarType {
+  DODGE, MUSTANG, CHEVROLET, BUICK;
+
+  public static CarType getRandColor() {
+    Random random = new Random();
+    int getRand = random.nextInt(CarColor.values().length);
+    return CarType.values()[getRand];
+  }
+
 }
