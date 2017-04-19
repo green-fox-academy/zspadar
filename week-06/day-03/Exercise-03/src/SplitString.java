8cc2f75735f4351f10cef5ae6078b59350a4199b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-19 16:18:28 +0200

    (delete) unnecessary comments

--- week-06/day-03/Exercise-03/src/SplitString.java
+++ week-06/day-03/Exercise-03/src/SplitString.java
@@ -11,7 +11,6 @@ public class SplitString {
     int numberOfChar = scanner.nextInt();
     System.out.println(Arrays.asList(getSplittedString(userInput,numberOfChar)));
 
-
   }
 
   public static String[] getSplittedString(String input, int number) {
@@ -27,11 +26,3 @@ public class SplitString {
   }
 
 }
-//  Write a function that receives a string and an integer
-//  The function should return an array of strings, with two string in it
-//        The first string should be the first part of the original string
-//        Characters in range [0,index-1].
-//        The second part should be the rest of the string characters range [index,length-1]
-//        Example: split("lovebarbara", 4)
-//        Result: {"love", "barbara"}
-//        Handle the exception if the index is out of bounds
