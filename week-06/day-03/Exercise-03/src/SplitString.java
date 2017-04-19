f2e9f541140a5924f35c0b45a68a1fcbb13bbb05
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-19 16:11:12 +0200

    (almost working splitted string...

--- week-06/day-03/Exercise-03/src/SplitString.java
+++ week-06/day-03/Exercise-03/src/SplitString.java
@@ -1,21 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 19..
  */
 public class SplitString {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     String userInput = scanner.nextLine();
     int numberOfChar = scanner.nextInt();
-    System.out.println(getSplittedString(userInput,numberOfChar));
+    System.out.println(Arrays.asList(getSplittedString(userInput,numberOfChar)));
 
 
   }
+
   public static String[] getSplittedString(String input, int number) {
     String[] splitted = new String[2];
-    splitted[1] = input.substring(0, number - 1);
-    splitted[2] = input.substring(number, input.length()-1);
+    try {
+      splitted[0] = input.substring(0, number - 1);
+      splitted[1] = input.substring(number, input.length()-1);
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println("The number is too big");
+    }
+
     return splitted;
   }
 
