9f78e309727b74429e60dac3f560ea0b6170fe2c
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-21 13:34:45 +0200

    (add) java class addCommand

---
+++ week-06/day-05/src/main/java/AddCommand.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 21..
+ */
+public class AddCommand extends Command {
+
+  AddCommand() {
+    super("a", "Adds a new task");
+  }
+
+  @Override
+  public String execute() {
+    return " ";
+  }
+}
