01fe633c55173a3d13bce773df245a8e8bd8da01
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-21 13:39:31 +0200

    (add) add and remove Command classes

---
+++ week-06/day-05/src/main/java/CompleteCommand.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 21..
+ */
+public class CompleteCommand extends Command {
+  CompleteCommand() {
+    super("c", "Completes an task");
+  }
+
+  @Override
+  public String execute() {
+    return "";
+  }
+}
---
+++ week-06/day-05/src/main/java/RemoveCommand.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 21..
+ */
+public class RemoveCommand extends Command{
+  RemoveCommand() {
+    super("r", "Removes an task");
+  }
+
+  @Override
+  public String execute() {
+    return "";
+  }
+}
