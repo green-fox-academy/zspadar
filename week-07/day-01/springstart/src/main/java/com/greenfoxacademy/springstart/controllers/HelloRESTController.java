a9cb4464ef52aa8e3b86faffbcf05621c8452bfe
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 13:51:13 +0200

    (feature) HelloRestController counts

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloRESTController {
+  AtomicLong id = new AtomicLong();
 
 
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam String name) {
-  Greeting greeting = new Greeting(1, String.format("Hello, %s,", name));
-  return greeting;
+    Greeting greeting = new Greeting(id.getAndIncrement(), String.format("Hello, %s", name));
+    return greeting;
   }
-
 
 
 }
