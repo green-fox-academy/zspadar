69d9a4d903d9e2c05a475170faaced001f5eba41
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 11:36:40 +0200

    (add) new java class HellorestController

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -7,4 +7,16 @@ public class Greeting {
   private long id;
   private String content;
 
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
 }
---
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 02..
+ */
+@RestController
+public class HelloRESTController {
+
+}
