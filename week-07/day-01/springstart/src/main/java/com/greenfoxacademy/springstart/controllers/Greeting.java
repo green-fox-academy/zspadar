f218a9cb52b6755cb3c44a5fbfefa6084ef59e0a
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 12:13:25 +0200

    (feat) localhost greeting works

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -1,19 +1,20 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by zsuzsanna.padar on 2017. 05. 02..
  */
+@Getter
+@Setter
+
+
 public class Greeting {
   private long id;
   private String content;
-
-  public long getId() {
-    return id;
-  }
-
-  public String getContent() {
-    return content;
-  }
 
   public Greeting(long id, String content) {
     this.id = id;
--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,5 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloRESTController {
+
+@RequestMapping(value = "/greeting")
+  public Greeting greeting() {
+  Greeting greeting = new Greeting(1, "Hello, World!");
+  return greeting;
+  }
 
 }
