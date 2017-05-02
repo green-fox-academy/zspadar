42c6db8f8536e7e2fdebf003fd5a79570c33629f
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 17:05:50 +0200

    (create) new project for last exercise

---
+++ week-07/day-01/springstart2/src/main/java/com/greenfoxacademy/springstart2/controllers/Greeting.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springstart2.controllers;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 02..
+ */
+public class Greeting {
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+}
