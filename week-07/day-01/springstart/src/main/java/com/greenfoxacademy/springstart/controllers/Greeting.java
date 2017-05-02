b86822e881deb7ce538b8ffbac94001dafdbdb9b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 11:32:44 +0200

    (add) Greeting java class to controller package

---
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.springstart.controllers;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 02..
+ */
+public class Greeting {
+  private long id;
+  private String content;
+
+}
