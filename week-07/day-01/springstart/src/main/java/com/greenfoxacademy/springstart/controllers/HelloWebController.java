784cfd7e36c0e8d7e6c765943701dae8c2153312
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-02 14:06:48 +0200

    (feat) greeting as a webpage

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,8 +1,20 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by zsuzsanna.padar on 2017. 05. 02..
  */
+@Controller
 public class HelloWebController {
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " World");
+    return "greeting";
+  }
 
 }
