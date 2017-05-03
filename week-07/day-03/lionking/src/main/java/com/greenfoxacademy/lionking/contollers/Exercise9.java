9a663d64662497b1d213439a91153108fc308a67
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-03 17:20:46 +0200

    (reafctor)xml in exercise8

---
+++ week-07/day-03/lionking/src/main/java/com/greenfoxacademy/lionking/contollers/Exercise9.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.lionking.contollers;
+
+import com.greenfoxacademy.lionking.model.BankAccount;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 03..
+ */
+@Controller
+public class Exercise9 {
+  @RequestMapping(value = "/Exercise9")
+  public String getSixthEndPoint(Model model) {
+
+    ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+    bankAccounts.add(new BankAccount("Simba", 2000, "lion"));
+    bankAccounts.add(new BankAccount("Rafiki", 55000, "chimpanzee"));
+    bankAccounts.add(new BankAccount("Nala", 11000, "lion"));
+    bankAccounts.add(new BankAccount("Ed", 1, "hyena"));
+    bankAccounts.add(new BankAccount("Zulu", 354, "toucan"));
+
+    model.addAttribute("bankAccounts",bankAccounts);
+
+    return "ninthEndPoint";
+  }
+
+}
