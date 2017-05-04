0044ef69b26c7fcc07f250e32807bf170a9b1d85
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-04 21:41:54 +0200

    navigating to localhost 8080

--- week-07/day-05/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/controller/MainController.java
+++ week-07/day-05/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/controller/MainController.java
@@ -1,8 +1,23 @@
 package com.greenfox.programmerfoxclub.controller;
 
+import com.greenfox.programmerfoxclub.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by zsuzsanna.padar on 2017. 05. 04..
  */
+@Controller
 public class MainController {
+  @Autowired
+  Fox fox;
+
+  @RequestMapping("/")
+  public String index(Model model){
+    model.addAttribute("fox", fox);
+    return "index";
+  }
 
 }
