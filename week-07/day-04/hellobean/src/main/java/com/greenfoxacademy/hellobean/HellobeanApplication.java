8bb22432911d15fcb6e868154fe4a46959c6b60b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-04 11:34:40 +0200

    (add) new project hellobean

---
+++ week-07/day-04/hellobean/src/main/java/com/greenfoxacademy/hellobean/HellobeanApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.hellobean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobeanApplication.class, args);
+	}
+}
