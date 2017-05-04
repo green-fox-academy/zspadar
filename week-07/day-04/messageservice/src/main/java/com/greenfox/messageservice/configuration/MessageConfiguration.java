def0fa03bc7589e9eeeb7159a7b74a47f4fbb714
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-04 20:29:39 +0200

    refactor(code annd delete unnecessary bean)

--- week-07/day-04/messageservice/src/main/java/com/greenfox/messageservice/configuration/MessageConfiguration.java
+++ week-07/day-04/messageservice/src/main/java/com/greenfox/messageservice/configuration/MessageConfiguration.java
@@ -5,17 +5,15 @@ import com.greenfox.messageservice.service.EmailService;
 import com.greenfox.messageservice.service.TwitterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Created by zsuzsanna.padar on 2017. 05. 04..
  */
 @Configuration
 public class MessageConfiguration {
-  @Bean
-  public MessageConfiguration messageConfiguration(){
-    return new MessageConfiguration();
-  }
 
+  @Primary
   @Bean
   public MessageService emailService() {
     return new EmailService();
