6887e1d41742834dd28722be9e7f3f88bd7ef3db
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-05 11:01:23 +0200

    (feat) food dirnk add methods

--- week-07/day-05/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/model/Nutrition.java
+++ week-07/day-05/programmerfoxclub/src/main/java/com/greenfox/programmerfoxclub/model/Nutrition.java
@@ -1,8 +1,20 @@
 package com.greenfox.programmerfoxclub.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zsuzsanna.padar on 2017. 05. 05..
  */
 public class Nutrition {
+  List<String> drinks = new ArrayList<>();
+  List<String> food = new ArrayList<>();
+
+  public void addFood(String userInput) {
+    food.add(userInput);
+  }
+  public void addDrink(String userInput){
+    drinks.add(userInput);
+  }
 
 }
