81c48b64a364587b61e8d71bc81f7f5081067463
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-09 12:04:35 +0200

    (init) first exercise

---
+++ week-08/day-02/listingtodos/src/test/java/com/greenfox/listingtodos/ListingtodosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.listingtodos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListingtodosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
