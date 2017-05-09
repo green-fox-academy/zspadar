82d0c59e829bd56dbe21bed83dba2a00a128f80d
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-09 22:26:08 +0200

    add(todo3.html):display true or false values

--- week-08/day-02/listingtodos/src/main/java/com/greenfox/listingtodos/controller/ToDoController.java
+++ week-08/day-02/listingtodos/src/main/java/com/greenfox/listingtodos/controller/ToDoController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -26,5 +27,8 @@ public class ToDoController {
     model.addAttribute("todos", toDoRepository.findAll());
     return "todo2";
   }
+
+  @RequestMapping(value = "/?isActive=true")
+  public String list2(@RequestParam())
 
 }
