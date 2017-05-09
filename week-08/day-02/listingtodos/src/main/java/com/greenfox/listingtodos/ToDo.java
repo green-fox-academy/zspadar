04200fadb63b11193397a53f6d097fb8e450a6c9
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-09 12:09:52 +0200

    add(ToDo class): init fields and constructor

---
+++ week-08/day-02/listingtodos/src/main/java/com/greenfox/listingtodos/ToDo.java
@@ -0,0 +1,24 @@
+package com.greenfox.listingtodos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 09..
+ */
+@Getter
+@Setter
+public class ToDo {
+  int id;
+  String title;
+  boolean isUrgent;
+  boolean isDone;
+
+  public ToDo(int id, String title, boolean isUrgent, boolean isDone) {
+    this.id = id;
+    this.title = title;
+    this.isUrgent = false;
+    this.isDone = false;
+  }
+  
+}
