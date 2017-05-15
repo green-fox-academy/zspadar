d57b452763099cf647d3f1081cc4d9903e8a2292
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-15 15:07:03 +0200

    add(/rocket endpoint):getmapping reqest works

---
+++ week-09/day-01/Exercise-01-Groot/src/main/java/com/greenfox/guardian/rora/controller/CargoController.java
@@ -0,0 +1,19 @@
+package com.greenfox.guardian.rora.controller;
+
+import com.greenfox.guardian.rora.model.Cargo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 15..
+ */
+@RestController
+public class CargoController {
+
+  @GetMapping(value = "/rocket")
+  public Cargo getStatus() {
+    Cargo cargo = new Cargo();
+    return cargo;
+  }
+
+}
---
+++ week-09/day-01/Exercise-01-Groot/src/main/java/com/greenfox/guardian/rora/model/Cargo.java
@@ -0,0 +1,40 @@
+package com.greenfox.guardian.rora.model;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 15..
+ */
+public class Cargo {
+  int caliber25;
+  int caliber30;
+  int caliber50;
+  String shipstatus;
+  boolean ready;
+
+  public Cargo() {
+    this.caliber25 = 0;
+    this.caliber30 = 0;
+    this.caliber50 = 0;
+    this.shipstatus = "empty";
+    this.ready = false;
+  }
+
+  public int getCaliber25() {
+    return caliber25;
+  }
+
+  public int getCaliber30() {
+    return caliber30;
+  }
+
+  public int getCaliber50() {
+    return caliber50;
+  }
+
+  public String getShipstatus() {
+    return shipstatus;
+  }
+
+  public boolean isReady() {
+    return ready;
+  }
+}
