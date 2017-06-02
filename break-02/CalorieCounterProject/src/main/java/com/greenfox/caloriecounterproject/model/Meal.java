9be2cf2216862d0910d8bb7c817452f3973a044d
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-02 11:55:52 +0200

    add(Meal.java class)

--- break-02/CalorieCounterProject/src/main/java/com/greenfox/caloriecounterproject/model/Meal.java
+++ break-02/CalorieCounterProject/src/main/java/com/greenfox/caloriecounterproject/model/Meal.java
@@ -1,7 +1,11 @@
 package com.greenfox.caloriecounterproject.model;
 
 
+import java.time.Instant;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +18,22 @@ import lombok.Setter;
 @Setter
 @Table(name = "Meal")
 public class Meal {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  Instant mealDate;
+  private MealType type;
+  private String description;
+  private double calories;
 
+  public Meal() {
+  }
 
-
+  public Meal(MealType type, String description, double calories) {
+    this.id = id;
+    this.mealDate = Instant.now();
+    this.type = type;
+    this.description = description;
+    this.calories = calories;
+  }
 }
