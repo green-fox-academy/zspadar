0a0550d99bcdcdf47532bfeac5bb74c8e7ee4106
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-02 11:15:25 +0200

    init(CalorieCounterProject):add dependencies, create packages for model controller and repositoery

---
+++ break-02/CalorieCounterProject/src/main/java/com/greenfox/caloriecounterproject/CaloriecounterprojectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.caloriecounterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriecounterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriecounterprojectApplication.class, args);
+	}
+}
