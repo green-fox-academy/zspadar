c33fc46e4f0f391f95a2d4a0ed016e5e4c88eab9
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-07-10 09:34:10 +0200

    init(main)

--- final-exam-warmup/src/Fifteen.java
+++ final-exam-warmup/src/Fifteen.java
@@ -1,7 +1,13 @@
+
+
 /**
  * Created by zsuzsanna.padar on 2017. 07. 10..
  */
 //Create a function that takes a string and splits it to a list of two
 // character strings like: "abcdef" becomes ["ab", "cd", "ef"]
 public class Fifteen {
+  public static void main(String[] args) {
+
+  }
 }
+
