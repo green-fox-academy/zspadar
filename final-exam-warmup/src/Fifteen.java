c6378ee1303444d25ab96179dd0d4d5e40107aac
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-07-10 09:51:31 +0200

    feat(splittedStringList): splits string into two letters

--- final-exam-warmup/src/Fifteen.java
+++ final-exam-warmup/src/Fifteen.java
@@ -1,13 +1,29 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zsuzsanna.padar on 2017. 07. 10..
  */
-//Create a function that takes a string and splits it to a list of two
-// character strings like: "abcdef" becomes ["ab", "cd", "ef"]
+
 public class Fifteen {
   public static void main(String[] args) {
-
+    System.out.println(splitStringToTwoChars("abcdef"));
   }
+
+  public static List<String> splitStringToTwoChars(String input) {
+    String tempWord = "";
+    int tempNumber = 0;
+    List<String> splittedList = new ArrayList<>();
+    for (int i = 0; i < input.length(); i++) {
+      tempNumber++;
+      tempWord += input.charAt(i);
+      if (tempNumber % 2 == 0) {
+        splittedList.add(tempWord);
+        tempWord = "";
+      }
+    }
+    return splittedList;
+  }
+
 }
 
